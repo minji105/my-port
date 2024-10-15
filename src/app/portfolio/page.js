cdb1961070bf9059e3c18196c571d5a5cd9477ad
minji105 <ajhjmj9@naver.com>
2024-10-15 16:13:14 +0900

    portfolio grid style

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -3,7 +3,85 @@ import styles from "./page.module.scss";
 export default function Portfolio() {
   return (
     <div className={styles.portfolio}>
-ss
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
+      
+      <div>
+        <div className={styles.title}>
+          <span>title</span>
+          <span>subject</span>
+        </div>
+        <div className={styles.image}>
+          image
+        </div>
+      </div>
     </div>
   );
 }
