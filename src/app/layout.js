535ced273f3967dcffc70b26d5f332e83d506d8d
minji105 <ajhjmj9@naver.com>
2024-10-17 00:29:51 +0900

    cursor custom

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import "./globals.css";
 import Nav from "./components/Nav";
+import Cursor from "./components/cursor";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Nav />
+        <Cursor />
         {children}
       </body>
     </html>
