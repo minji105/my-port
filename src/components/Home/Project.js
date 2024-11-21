66e60bc2a6b13fce80b9b725929e792649a01d71
minji105 <ajhjmj9@naver.com>
2024-11-21 16:53:16 +0900

    project section separate

---
+++ src/components/Home/Project.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import styles from './Sections.module.scss';
+import projectList from '../../data/projectList.json';
+
+export default function Sections() {
+
+  const [projectImg, setProjectImg] = useState(projectList[0].hover);
+  const [projectStack, setProjectStack] = useState(projectList[0].stack);
+  const [selectedRow, setSelectedRow] = useState(0);
+
+  const handleClick = (index, img, stack) => {
+    setProjectImg(img);
+    setProjectStack(stack);
+    setSelectedRow(index);
+  }
+
+  return (
+    <div className={styles.project}>
+      <div>
+        {projectList.map((project, index) => (
+          <div
+            key={index}
+            className={`${styles.row} ${selectedRow === index ? styles.selected : ''} ${index + 1 === projectList.length ? styles.after : ''}`}
+            onClick={() => handleClick(index, project.hover, project.stack)}>
+            <div className={styles.title}>
+              <p>{project.title}</p>
+            </div>
+            <div>
+              <p>{project.type}</p>
+              <p>{project.category}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.projectDetail}>
+        <img src={projectImg} alt="project img" />
+        <div className={styles.projectInfo}>
+          <p>{projectStack}</p>
+          <div className={styles.viewIcon}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" width="20px">
+              <path stroke="#000" stroke-width="2" d="M11 1H1v10M15 1h10v10M15 25h10V15M11 25H1V15m12-8v12m6-6H7"></path>
+            </svg>
+            <p>view the project</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Home/Sections.js
+++ src/components/Home/Sections.js
@@ -2,19 +2,9 @@ import React, { useState } from 'react';
 import styles from './Sections.module.scss';
 import Skills from './Skills';
 import Resume from './Resume';
-import projectList from '../../data/projectList.json';
+import Project from './Project';
 
 export default function Sections() {
-  const last_index = projectList.length - 1;
-
-  const [projectImg, setProjectImg] = useState(projectList[0].hover);
-  const [selectedRow, setSelectedRow] = useState(0);
-
-  const handleClick = (index, img) => {
-    setProjectImg(img);
-    setSelectedRow(index);
-  }
-
   return (
     <div className={styles.container}>
       <section>
@@ -36,28 +26,7 @@ export default function Sections() {
       <section>
         <h2>Project</h2>
 
-        <div className={styles.project}>
-          <div>
-            {projectList.map((project, index) => (
-              <div
-                key={index}
-                className={`${styles.row} ${selectedRow === index ? styles.selected : ''}`}
-                onClick={() => handleClick(index, project.hover)}>
-                <p>{project.title}</p>
-                <p>{project.category}</p>
-              </div>
-            ))}
-          </div>
-          <div className={styles.projectDetail}>
-            <img src={projectImg} alt="project img" />
-            <div className={styles.viewIcon}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" width="20px">
-                <path stroke="#000" stroke-width="2" d="M11 1H1v10M15 1h10v10M15 25h10V15M11 25H1V15m12-8v12m6-6H7"></path>
-              </svg>
-              <p>view the project</p>
-            </div>
-          </div>
-        </div>
+        <Project />
       </section>
     </div>
   )
