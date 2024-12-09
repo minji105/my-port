1873451da482732d60a48362d3dbd3d2b0ac87c3
minji105 <ajhjmj9@naver.com>
2024-12-09 18:14:04 +0900

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,10 @@ function App({ Component, pageProps }) {
       <Head>
         <title>Resume: 조민지</title>
         <meta name="description" content="개발자 조민지의 포트폴리오입니다."></meta>
-        <meta name="keywords" content="프론트엔드 포트폴리오, 개발자 포트폴리오, 웹 개발자, 포트폴리오 사이트"></meta>
+        <meta name="application-name" content="Resume: 조민지"></meta>
+        <meta name="author" content="조민지"></meta>
+        <meta name="keywords" content="조민지, 포트폴리오, 개발자, 프론트엔드, portfolio"></meta>
+        <meta name="google-site-verification" content="YT2ed87ec-uxvMV765Y78BYLFEs1CKVUe7xi8VH6JVw" />
         <meta property="og:title" content="Resume: 조민지"></meta>
         <meta property="og:description" content="개발자 조민지의 포트폴리오입니다."></meta>
       </Head>
