0d899ae7833d62102b3bc187f42231868c1c3b99
minji105 <ajhjmj9@naver.com>
2024-12-09 16:44:31 +0900

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,4 @@
 import './global/App.scss';
-// import Home from './components/Home';
-// import Joheesu from './components/Projects/Joheesu';
-// import HandTracking from './components/Projects/HandTracking';
-// import HotelMetaverse from './components/Projects/HotelMetaverse';
-// import Cyworld from './components/Projects/Cyworld';
-// import MyPT from './components/Projects/MyPT';
-import Head from 'next/head';
 
 function App({ Component, pageProps }) {
   return (
@@ -18,16 +11,6 @@ function App({ Component, pageProps }) {
         <meta property="og:description" content="개발자 조민지의 포트폴리오입니다."></meta>
       </Head>
       <Component {...pageProps} />
-      {/* <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project/Photographer-Portfolio" element={<Joheesu />} />
-          <Route path="/project/HandTracking" element={<HandTracking />} />
-          <Route path="/project/Hotel-Metaverse" element={<HotelMetaverse />} />
-          <Route path="/project/Cyworld" element={<Cyworld />} />
-          <Route path="/project/MyPT" element={<MyPT />} />
-        </Routes>
-      </Router> */}
     </>
   );
 }
